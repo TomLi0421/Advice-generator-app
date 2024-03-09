ed8f17a22d28edaea27e3005c23ac2881136351e
Tom <tom.cyli50@gmail.com>
2024-03-09 11:04:36 +0800

    fix: vite.config.js base name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Advice-generator-app/",
+  base: "/advice-generator-app/",
 });
